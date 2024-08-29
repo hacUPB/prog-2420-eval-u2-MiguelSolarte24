e50ce24869c9452bfc697c1fbc0901d0e1f2bde5
MiguelSolarte24 <miguelangelsolartep@gmail.com>
2024-08-28 23:28:45 -0500

    Update satelite.py

--- src/satelite.py
+++ src/satelite.py
@@ -1,8 +1,16 @@
+Ingrese la altitud inicial del satélite (en kilómetros): 320Km
+Introduzca el coeficiente de arrastre inicial: 0.2
+Ingrese la altitud mínima segura (en kilómetros): 50Km
 
-def main():
-    #Tu código va aquí
-    pass # borra esta línea cuando con inicies tu código
+Calculamos la altitud perdidaa: 
+Altitud_perdida = coeficiente_arrastre * altitud_actual.
 
+Restamos la pérdida de altitud a la altitud actual.
 
-if __name__ == "__main__":
-    main()
+Se aumenta el coeficiente de arrastre =  coeficiente_arrastre += 0.0001
+
+Órbita 1: Altitud actual = 320 km, Coeficiente de arrastre = 0.2
+Órbita 2: Altitud actual = 322,15 km, Coeficiente de arrastre = 0.3
+Órbita 20: Altitud actual = 250.500 km, Coeficiente de arrastre = 0.050
+
+El satélite ha reingresado en la atmósfera terrestre después de 70 órbitas.
