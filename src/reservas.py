269c4456d3faf98ae469e521c136c048c3d1c14b
MiguelSolarte24 <miguelangelsolartep@gmail.com>
2024-08-28 23:21:10 -0500

    reservas.py

--- src/reservas.py
+++ src/reservas.py
@@ -1,8 +1,16 @@
+Buenos días¡¡ bienvenido a Latam Airlines 
+Me permites tu documento de identidad
+Nombre del pasajero: Miguel
+Apellido del pasajero: Solarte
 
-def main():
-    #Tu código va aquí
-    pass # borra esta línea cuando con inicies tu código
+Señor Miguel, tenemos una amplia variedad de rutas directa desde Medellín:  ( Bógota, Cali, Pereira, Santa Marta, Barranquilla, Cartagena, Bucaramanga)
+Cual de estos es su destino preferido? 
+Ingresa tu origen : Medellín 
+Ingresa tu destino: Barranquilla 
+La fecha del viaje seria el: Sabado 
+Intoduzca el dia del mes :  (12-10)
 
-
-if __name__ == "__main__":
-    main()
+Tu vuelo de Medellin a Barranquilla quedó reservado para el sábado 10 de octubre a las 9:10am.
+Precio del tiquete: 120.000
+Tu silla es la: 20C
+Feliz viaje 
