fe1ef36937beed12009aa7ad7827b64c878a5249
MiguelSolarte24 <miguelangelsolartep@gmail.com>
2024-08-29 09:30:47 -0500

    reservas.py

--- src/reservas.py
+++ src/reservas.py
@@ -1,16 +1,30 @@
-Buenos días¡¡ bienvenido a Latam Airlines 
-Me permites tu documento de identidad
-Nombre del pasajero: Miguel
-Apellido del pasajero: Solarte
 
-Señor Miguel, tenemos una amplia variedad de rutas directa desde Medellín:  ( Bógota, Cali, Pereira, Santa Marta, Barranquilla, Cartagena, Bucaramanga)
-Cual de estos es su destino preferido? 
-Ingresa tu origen : Medellín 
-Ingresa tu destino: Barranquilla 
-La fecha del viaje seria el: Sabado 
-Intoduzca el dia del mes :  (12-10)
+Muy buenos días, bienvenido a Avianca!!
 
-Tu vuelo de Medellin a Barranquilla quedó reservado para el sábado 10 de octubre a las 9:10am.
-Precio del tiquete: 120.000
-Tu silla es la: 20C
-Feliz viaje 
+Me permites tu documento de identidad, tu nombre y apellidos. 
+
+Sr Miguel Solarte, Bienvenido a Avianca!!!!
+
+In(origen)
+In " Escribe el origen que deseas " 
+print "Medellín"
+In " Escribe el destino al que deseas ir"
+print "Cartagena"
+
+In "Sr Miguel que día de la semana desaría viajar"
+print " El sabado "
+
+len(dia del mes)
+Introduce el dia del mes: (12-10)
+
+Numero de Asiento deseado:
+random.randint(1, 29)
+Introduce el numero de asiento: (15A)
+
+len(horario)
+Introduce el horario deseado: (9:15am)
+
+Len(costo del tiquete)
+Introduce el costo: ($90.000)
+
+Imprimir ("Tu tiquete de Medellin a Cartagena esta reservado para el sabado 12 de octubre a las 9:15am")
